5382609346eee75cb851ea3f4493475031e14186
Artem Poliakov <artem4250@gmail.com>
2023-05-06 07:39:42 +0300

    Position, Token minor update

--- os/src/main/java/os/lexer/Position.java
+++ os/src/main/java/os/lexer/Position.java
@@ -1,6 +1,5 @@
 package os.lexer;
 
-
 public record Position(int line, int column) {
     @Override
     public String toString() {
--- os/src/main/java/os/lexer/Token.java
+++ os/src/main/java/os/lexer/Token.java
@@ -1,6 +1,5 @@
 package os.lexer;
 
-
 public record Token(TokenType type, String value, Position position) {
     @Override
     public String toString() {
